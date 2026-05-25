6edd0afe61f3d01fbde756b1fe3ea7064fe93c52
Sahil <sahilchoudhary1615@gmail.com>
2026-05-25 07:02:05 +0530

    first commit

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -15,7 +15,7 @@ function AuthModel({onClose}) {
     },[userData , onClose])
 
   return (
-    <div className='fixed inset-0 z-[999] flex items-center justify-center bg-black/10 backdrop-blur-sm px-4'>
+    <div className='fixed inset-0  flex items-center justify-center bg-black/10 backdrop-blur-sm px-4'>
         <div className='relative w-full max-w-md'>
             <button onClick={onClose} className='absolute top-8 right-5 text-gray-800 hover:text-black text-xl'>
              <FaTimes size={18}/>
