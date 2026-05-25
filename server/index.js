b004dae7fecef53e52b82efc907504f1b00a2867
sahil-dev75 <ksahil85372@gmail.com>
2026-05-25 08:03:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewiq-hkom.onrender.com",
     credentials:true
 }))
 
